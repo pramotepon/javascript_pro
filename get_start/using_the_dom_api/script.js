0818ceedda3575e6c1c27ef75ec9a9d217d169a2
Zeekerz <pramote.p.on@gmail.com>
2023-03-18 00:06:19 +0700

    Create new tag p, textContent, appendChild

--- get_start/using_the_dom_api/script.js
+++ get_start/using_the_dom_api/script.js
@@ -1,5 +1,13 @@
-// DOM API 
+/* DOM API */
 // Element id innerHTML สามารถแสดงข้อมูลโดยการใส่แท็ก HTML ได้ 
 document.getElementById('paragraph_inner').innerHTML = '<b>Hello World! by innerHTML</b>';
 // Element id textContent แสดงได้แต่ข้อความ รันแท็ก HTML ไม่ได้ 
 document.getElementById('paragraph_text').textContent = '<b>Hello World! by textContent</b>';
+
+/* Create new tag p with a textContent property of and add it at the end of the html body */
+// Create new tag p into variable element
+let element = document.createElement('p');
+// Variable element Insert content by textContent
+element.textContent = "Hello, World by Create new Element";
+//add the newly created element to the DOM
+document.body.appendChild(element);
