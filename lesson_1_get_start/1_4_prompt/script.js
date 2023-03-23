b54774486aa8b130e781bb7f23b64f9e79804855
Zeekerz <pramote.p.on@gmail.com>
2023-03-24 00:43:39 +0700

    window.prompt

---
+++ lesson_1_get_start/1_4_prompt/script.js
@@ -0,0 +1,11 @@
+/* 
+    Using window.prompt()
+*/
+// prompt(text, [default]);
+// text: The text displayed in the prompt box.
+// default: A default value for the input field (optional). ('')
+
+// let age = prompt('How old are you?', 30);
+let age = prompt('How old are you?');
+
+console.log(age);
