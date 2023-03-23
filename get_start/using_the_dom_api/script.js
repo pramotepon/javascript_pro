08a9bfad3f1e23eaf05c2512a3f400799ac39208
Zeekerz <pramote.p.on@gmail.com>
2023-03-23 22:19:29 +0700

    Using window.alert

--- get_start/using_the_dom_api/script.js
+++ get_start/using_the_dom_api/script.js
@@ -11,3 +11,21 @@ let element = document.createElement('p');
 element.textContent = "Hello, World by Create new Element";
 //add the newly created element to the DOM
 document.body.appendChild(element);
+
+/* 
+    Using window.alert
+*/
+// assign 'Hello World' to message Variable
+// let message = 'Hello World';
+// alert message on browser // Full Code
+// window.alert(message);
+// alert message on browser // Shorthand Code
+// Because window is the global object, you can call also use the following shorthand:
+/* The alert method is technically a property of window object, but since all window properties are
+automatically global variables, we can use alert as a global variable instead of as a property of window -
+meaning you can directly use alert() instead of window.alert(). */
+// alert(message);
+
+/* 
+    Using window.prompt
+*/
