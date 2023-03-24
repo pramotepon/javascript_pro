b9b3f783afd18a47a803481225776042512efd02
Zeekerz <pramote.p.on@gmail.com>
2023-03-24 12:31:47 +0700

    null

---
+++ leasson_3_built-in_constants/null/script.js
@@ -0,0 +1,11 @@
+/* 
+     Null
+*/
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
+console.log(typeof null); // object
+
+let a = null;
+
+console.log(a === null); // true
