c20ff2709e5499a7f3cda64b4e55b20e8b406c90
Zeekerz <pramote.p.on@gmail.com>
2023-03-24 10:30:37 +0700

    define variable

---
+++ lesson_2_variables/2_1_defining_a_variable/script.js
@@ -0,0 +1,16 @@
+// call variable var 
+var myVar = "Hello Var";
+/* 
+    JS Es6 ++
+*/
+// call variable let 
+let myLet = "Hello Let";
+// call variable const 
+const myCon = "Hello Const";
+
+/* 
+            Scope       New assign  Resign  
+    var     Block       No          Yes
+    let     Block       Yes         Yes
+    const   Function    Yes         Yes
+*/
---
+++ lesson_2_variables/2_2_using_a_variable/script.js
@@ -0,0 +1,19 @@
+// Variable number1 assign value = 1
+let number1 = 1;
+// Resign value number1 to 3
+number1 = 3;
+
+// Log number 1
+console.log(number1);
+
+// number1 Calculate number1 + 5
+number1 = number1 + 5; // 3 + 5 = 8
+// number1 Calculate number1 - 6
+number1 = number1 - 6; // 8 - 6 = 2
+// Variable number2 assign value = number1 * 10
+let number2 = number1 * 10; // 2 * 10 = 20
+// Variable number3 assign value = number2 / number1
+let number3 = number2 / number1; // 20 / 2 = 10
+
+// With String
+let myString = "Hello " + "World"; // Hello World
