545097aff6624b2d94b25537916ef2cdf1f5712e
Zeekerz <pramote.p.on@gmail.com>
2023-03-26 21:31:35 +0700

    datatypes_in_javascrip

---
+++ lesson_3_built-in_constants/datatypes_in_javascrip/script.js
@@ -0,0 +1,30 @@
+/* 
+    1. String
+*/
+console.log(typeof "String");
+console.log(typeof Date(2011,01,01));
+/* 
+    2. Numbers
+*/
+console.log(typeof 42);
+/* 
+    3. Boolean
+*/
+console.log(typeof true);
+/* 
+    4. Object
+*/
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof null);
+console.log(typeof /aaa/);
+console.log(typeof Error());
+/* 
+    5. Function
+*/
+console.log(typeof function(){});
+/* 
+    6. Undefined
+*/
+let var1;
+console.log(typeof var1);
